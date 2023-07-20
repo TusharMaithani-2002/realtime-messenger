ac9d3e0c6bdb34e29fc1cea241cb8db17215484e
TusharMaithani-2002 <202051194@iiitvadodara.ac.in>
2023-07-20 22:56:27 +0530

    added next-superjosn-plugin swcplugin

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental:{
+    
+        swcPlugins: [
+            //sanities objects so that we can safely pass them even though they have date objects and other complex properties
+            // which cannot be passed from server component to client component
+            // this removes an error
+            ["next-superjson-plugin",{}]
+        ]
+    }
+}
 
 module.exports = nextConfig
